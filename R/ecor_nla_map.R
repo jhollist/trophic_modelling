18e6345f7ebe31739239a640111dbf2518f0ab28
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-11-23 11:26:03 -0500

    working on figure

--- R/ecor_nla_map.R
+++ R/ecor_nla_map.R
@@ -17,13 +17,18 @@
 #' library(autocrop)
 #'
 #' wsa9 <- readOGR(system.file("extdata",package="LakeTrophicModelling"),"wsa9_low48")
+#' wsa9 <- wsa9[!is.na(wsa9$WSA_9),]
+#' wsa9$WSA_9 <- c("Coastal\nPlains (CPL)","Northern\nAppalachians (NAP)", 
+#'           "Northern\nPlains (NPL)", "Southern\nAppalachians (SAP)",
+#'           "Southern\nPlains (SPL)", "Temperate\nPlains (TPL)", 
+#'           "Upper\nMidwest (UMW)", "Western\nMountains (WMT)", "Xeric (XER)")
 #' nla <- read.csv(system.file("extdata/ltmData.csv",package="LakeTrophicModelling"))
 #' lakes_alb<-data.frame(nla[["AlbersX"]],nla[["AlbersY"]]) 
 #' p4s<-"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" 
 #' lakes_alb_sp<-SpatialPoints(coordinates(lakes_alb),proj4string=CRS(p4s))
 #' 
-#' ggsave(plot=ecor_nla_map(wsa9,lakes_alb_sp),filename="ecor_nla_map.tiff",width=8.5)
-#' autocrop("ecor_nla_map.tiff",10,"ecor_nla_map.tiff")
+#' ggsave(plot=ecor_nla_map(wsa9,lakes_alb_sp),filename="ecor_nla_map.jpg",width=8.5)
+#' autocrop("ecor_nla_map.jpg",10,"ecor_nla_map.jpg")
 #' 
 #' @export
 #' @import ggplot2
@@ -34,24 +39,30 @@ ecor_nla_map<-function(ecor,nla_pts){
   names(ecor_f)[which(names(ecor_f)=="id")] = "WSA_9"
   ecor_df <- plyr::join(ecor_f, ecor@data, by="WSA_9")
   nla_dd<-spTransform(nla_pts,
-                        CRS=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+                      CRS=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
   nla_dd<-data.frame(coordinates(nla_dd))
   names(nla_dd)<-c("long","lat")
   gmap<-ggplot(ecor_df, aes(long,lat,group=group,fill=WSA_9)) +
-    geom_polygon(data=subset(ecor_df,WSA_9!="Western Mountains (WMT)")) +
-    geom_polygon(data=subset(ecor_df,WSA_9=="Western Mountains (WMT)")) +
-    geom_path(color="white") +
+    geom_polygon(data=subset(ecor_df,WSA_9!="Western\nMountains (WMT)")) +
+    geom_polygon(data=subset(ecor_df,WSA_9=="Western\nMountains (WMT)")) +
+    geom_path(color="lightgrey") +
     geom_point(data = nla_dd, aes(x=long,y=lat,group="",fill=""),size=1.5) +
     coord_equal() +
     coord_map("albers", lat2 = 45.5, lat1 = 29.5)+
     theme(panel.background = element_rect(fill="white"), panel.grid = element_blank(),
-          panel.border = element_blank(), #legend.position = "none",
+          panel.border = element_blank(), 
           axis.text = element_blank(),axis.ticks = element_blank(),
           legend.text = element_text(size=10),
-          legend.key.width = unit(0.5, "line")) +
+          legend.position = c(0.96,0.5),
+          #legend.justification = c(0,0),
+          legend.key.width = unit(0.5, "line"),
+          legend.key.height = unit(1.6, "line"),
+          legend.margin = unit(0,"line"),
+          plot.margin = unit(c(1,3,0.5,0.5),"line")
+    ) +
     ylab("") +
     xlab("") +
     scale_fill_manual(name="",values=viridis(10))
-    
+  
   return(gmap)
 }
